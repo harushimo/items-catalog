20acb324432efbb51634f8de0002edf96749bfc0
Hari Vedam <hvedam@gmail.com>
2017-03-26 11:27:09 -0500

    working updating paths

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -12,8 +12,7 @@ pip install itsdangerous
 pip install flask-httpauth
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
+su vagrant -c 'createdb sportsvenue'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
